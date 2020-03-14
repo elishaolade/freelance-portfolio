09f98537c2844b0a23a5698e37721bda8270cfa7
elishaolade <elishaolade@gmail.com>
2020-03-14 08:21:37 -0700

    added arrows to accordian for both mobile and desktop

--- js/main.js
+++ js/main.js
@@ -11,15 +11,22 @@ $(function(){
         panel.style.maxHeight = null;
       }
       else {
+
+        /* Find all elements with panel class name */
         $('.panel').filter(function ( element ) {
+
+          /* Return elements with a max-height other than 0*/
+          /* Set the max-height of these elements to 0 */
           return !$(this).css('max-height','0')
         }).css('max-height','0');
+
+        /* Set max-height to scroll height to open panel*/
         panel.style.maxHeight = panel.scrollHeight + "px";
       }
     }
   })
 
-  $('.arrow').on({
+  $('.m-header-arrow').on({
     click: function(){
       $(this).toggleClass('active');
       $('.m-header').toggleClass('active');
